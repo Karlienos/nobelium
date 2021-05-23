19654f4cea3d341fc8f3c8f1b698d786be38be6a
Karlienos <84653472+Karlienos@users.noreply.github.com>
2021-05-23 14:23:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Karlienos',
   author: 'MaxYu',
   email: 'k.karlienos@gmail.com',
-  link: '',
+  link: 'https://www.notion.so/137e13f5ef7d42d1919ba3b2ba8cd8d1?v=b8a9e234f271423e808c7caf80d77fea',
   description: 'Read Only',
   lang: 'CHS',
   appearance: 'auto', // ['light', 'dark', 'auto'],
